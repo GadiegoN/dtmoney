5999bbef8267e292062b1249cd27f27879433e6a
GadiegoN <ngadiego@gmail.com>
2021-08-10 22:54:49 -0300

    :white_check_mark: Finalizando calculo de valores

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,12 @@
 import Modal from 'react-modal';
+import { Container, RadioBox, TransactionTypeContainer  } from './styles';
+import { FormEvent, useState, useContext } from 'react';
+import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
+
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
-import { Container, RadioBox, TransactionTypeContainer  } from './styles';
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -12,24 +14,29 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    const { createTransaction } = useContext(TransactionsContext);
+    
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [ amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
     //function handleSetTypeDeposit() {}
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {
+        await createTransaction({
             title,
-            value,
+            amount,
             category,
             type,
-        }
-
-        api.post('/transactions', data);
+        })
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
     
     return (
@@ -58,8 +65,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <input 
                     placeholder="Valor"
                     type="number"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
